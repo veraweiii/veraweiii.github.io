8d3abe0463dee5f9a3fa814c66a1d6ee35e7faa7
veraweiii <vera.wei@gmail.com>
2021-10-24 23:58:37 -0400

    Moved files

--- README.txt
+++ README.txt
@@ -1,2 +1,19 @@
-Extra Credit:
-Implemented product detail pages for all products
+Image Citations:
+- https://www.cookingclassy.com/better-than-cinnabon-cinnamon-rolls/
+- http://thesweetandsimplekitchen.com/lazy-blackberry-morning-buns/
+- https://cloudykitchen.com/blog/pumpkin-cinnamon-rolls/
+- http://angiesrecipes.blogspot.com/2020/04/cinnamon-walnut-rolls.html
+- https://www.loveandlemons.com/cinnamon-rolls/
+- https://tutti-dolci.com/caramel-pecan-cinnamon-rolls/
+- https://www.freepik.com/free-icon/cinnamon-roll_14647474.htm#page=1&query=cinnamon%20roll&position=27&from_view=search
+
+Font Citations:
+- https://www.fontfabric.com/fonts/intro-rust/#:~:text=Intro%20Rust%20is%20one%20of,the%20market%2C%20including%20214%20fonts.&text=A%20beautiful%20mixture%20of%20display,design%2C%20branding%2C%20and%20more.
+- https://fonts.google.com/specimen/Barlow
+
+New Features for 6A:
+- Price on product detail changes when user makes different size/quantity selections
+- Image on product detail changes when user makes different glazing selections
+- Note that the functionality above is only implemented for the "Original" and "Pumpkin Spice" flavors
+- Cart button in the upper-right corner has a numeric indicator showing how many items are currently in cart
+- Implemented static cart page, with dropdown selectors and buttons whose functionality will be added in 6B
---
+++ main.js
@@ -0,0 +1,135 @@
+window.onLoad = () => {
+    updateCartLabel();
+}
+
+// constructor for product
+function roll(type, size, glaze, price, img) {
+    this.type = type;
+    this.size = size;
+    this.glaze = glaze;
+    this.price = price;
+    this.img = img;
+}
+
+
+// Update number of items in cart
+function updateCartLabel() {
+    cartArray = localStorage.getItem("cartArray");
+    cartArray = JSON.parse(cartArray);
+    if (cartArray == null) {
+        cartArray = [];
+        // localStorage.setItem("cartArray", JSON.stringify(cartArray));
+    }
+    let cartSize = cartArray.length;
+    if (cartArray.length > 0) {
+        console.log(cartArray.length);
+        document.getElementById("cart-number").innerHTML = cartSize;
+    }
+    //document.getElementById("cart-number")[0].innerHTML = cartSize;
+}
+updateCartLabel();
+
+// Update price when size selected
+document.getElementById("size").addEventListener("change", function () {
+    if (document.getElementById("1-roll").checked) {
+        document.getElementsByClassName("price")[0].innerHTML = "$2.99";
+    } else if (document.getElementById("3-rolls").checked) {
+        document.getElementsByClassName("price")[0].innerHTML = "$8.99";
+    } else if (document.getElementById("6-rolls").checked) {
+        document.getElementsByClassName("price")[0].innerHTML = "$11.99";
+    } else if (document.getElementById("12-rolls").checked) {
+        document.getElementsByClassName("price")[0].innerHTML = "$23.99";
+    }
+});
+
+// Update image when glazing selected
+
+if (document.getElementsByClassName("item-header")[0].innerHTML == "Original") {
+    document.getElementById("glazing").addEventListener("change", function () {
+        if (document.getElementById("none").checked) {
+            document.getElementsByClassName("item-image")[0].src = "img/original.png";
+        } else if (document.getElementById("double-chocolate").checked) {
+            document.getElementsByClassName("item-image")[0].src = "img/glaze/original-chocolate.png";
+        } else if (document.getElementById("vanilla-milk").checked) {
+            document.getElementsByClassName("item-image")[0].src = "img/glaze/original-vanilla.png";
+        } else if (document.getElementById("sugar-milk").checked) {
+            document.getElementsByClassName("item-image")[0].src = "img/glaze/original-sugar.png";
+        }
+        //console.log(document.getElementById("none").checked);
+    });
+} else if (document.getElementsByClassName("item-header")[0].innerHTML == "Pumpkin Spice") {
+    document.getElementById("glazing").addEventListener("change", function () {
+        if (document.getElementById("none").checked) {
+            document.getElementsByClassName("item-image")[0].src = "img/pumpkin-spice.png";
+        } else if (document.getElementById("double-chocolate").checked) {
+            document.getElementsByClassName("item-image")[0].src = "img/glaze/pumpkin-chocolate.png";
+        } else if (document.getElementById("vanilla-milk").checked) {
+            document.getElementsByClassName("item-image")[0].src = "img/glaze/pumpkin-vanilla.png";
+        } else if (document.getElementById("sugar-milk").checked) {
+            document.getElementsByClassName("item-image")[0].src = "img/glaze/pumpkin-sugar.png";
+        }
+        //console.log(document.getElementById("none").checked);
+    });
+}
+
+
+
+// Add item to cart
+document.getElementById("cart-btn").addEventListener("click", function () {
+    let size;
+    let price;
+    if (document.getElementById("1-roll").checked) {
+        size = "1-roll";
+        price = "$2.99";
+    } else if (document.getElementById("3-rolls").checked) {
+        size = "3-rolls";
+        price = "$8.99";
+    } else if (document.getElementById("6-rolls").checked) {
+        size = "6-rolls";
+        price = "$11.99";
+    } else if (document.getElementById("12-rolls").checked) {
+        size = "12-rolls";
+        price = "$23.99";
+    }
+
+    let glaze;
+    let img;
+    if (document.getElementById("none").checked) {
+        glaze = "none";
+        img = "img/original.png";
+    } else if (document.getElementById("double-chocolate").checked) {
+        glaze = "double-chocolate";
+        img = "img/glaze/original-chocolate.png";
+    } else if (document.getElementById("vanilla-milk").checked) {
+        glaze = "vanilla-milk";
+        img = "img/glaze/original-vanilla.png";
+    } else if (document.getElementById("sugar-milk").checked) {
+        glaze = "sugar-milk";
+        img = "img/glaze/original-sugar.png";
+    }
+
+    let newRoll = new roll(
+        document.getElementsByClassName("item-header")[0].innerHTML,
+        size,
+        glaze,
+        price,
+        img
+    );
+
+    cartArray = JSON.parse(localStorage.getItem("cartArray"));
+    
+    if (cartArray == null) {
+        console.log("thiscartArray == null");
+        cartArray = [];
+        localStorage.setItem("cartArray", JSON.stringify(cartArray))
+    }
+    cartArray.push(newRoll);
+    localStorage.setItem("cartArray", JSON.stringify(cartArray));
+    console.log(cartArray);
+    updateCartLabel();
+});
+
+
+
+
+
